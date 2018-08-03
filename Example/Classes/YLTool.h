e608439d442bf799f1f823cddd162d1d705e4f8f
yoolooo <864275697@qq.com>
2018-08-03 11:51:50 +0800

    add a YLTool

---
+++ Example/Classes/YLTool.h
@@ -0,0 +1,13 @@
+//
+//  YLTool.h
+//  ylpodtest_Example
+//
+//  Created by yan on 2018/8/3.
+//  Copyright © 2018年 yoolooo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YLTool : NSObject
+
+@end
---
+++ Example/Classes/YLTool.m
@@ -0,0 +1,13 @@
+//
+//  YLTool.m
+//  ylpodtest_Example
+//
+//  Created by yan on 2018/8/3.
+//  Copyright © 2018年 yoolooo. All rights reserved.
+//
+
+#import "YLTool.h"
+
+@implementation YLTool
+
+@end
